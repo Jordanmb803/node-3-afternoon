bf157e0145c4c37e72505b9de025a3c964ae6e56
Jordan Buonforte <jordan@Jordans-MacBook-Air.local>
2018-05-08 14:03:53 -0600

    step 4 complete

--- server/index.js
+++ server/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressSession = require('express-session');
 require('dotenv').config();
+const checkForSession = require('./middlewares/checkForSession');
 
 const app = express();
 const port = 3555;
 
 app.use(bodyParser.json());
-
-
+app.session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+})
+app.use(checkForSession)
 
 
 app.listen(port, () => console.log(`Port ${port} now listening`))
